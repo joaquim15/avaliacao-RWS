63bc42cf49ae345682ea079ba1ae40b44010f635
joaquim.moura <joaquim_castro_moura@hotmail.com>
2020-07-17 12:33:22 -0300

    Criacao da entidade estudante e configuracao do arquivo properties.

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -1,5 +1,77 @@
 package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
-public class Estudante {
-	// TODO Implementar a entidade Estudante conforme solicitado
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity(name = "tb_estudante")
+public class Estudante implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@NotNull(message = "Nome é obrigatório")
+	@Column(name = "nome")
+	private String nome;
+
+	@NotNull(message = "Email é obrigatório")
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "telefone")
+	private String telefone;
+
+	public Estudante() {
+		super();
+	}
+
+	public Estudante(Integer id, String nome, String email, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 }
