103c632f044dc7e333f79a8bc0499994c09ef566
joaquim.moura <joaquim_castro_moura@hotmail.com>
2020-07-18 12:53:39 -0300

    Script SQL para fazer a carga automática.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
---EFETUE A CRIACAO DOS SCRIPTS QUE IRAO FAZER A CARGA DOS DADOS
+INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (0,'Xawoy', 'xawoy@tms.com.br', '7777', '123456', 'Engenharia da Computação');
+INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (1,'Furae', 'furae@tms.com.br', '7778', '678912', 'Ciência da Computação');
+INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (2,'Fupuy', 'fupuy@tms.com.br', '7779', '321654', 'Engenharia da Computação');
+INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (3,'Kuer', 'kuer@tms.com.br', '7780', '654987', 'Análise de Dados');
+INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (4,'Blias', 'blias@tms.com.br', '7781', '666131', 'Ciência da Computação');
