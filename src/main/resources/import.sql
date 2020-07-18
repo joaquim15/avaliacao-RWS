a5133101769f329e36406423723a15150abf6d5b
joaquim.moura <joaquim_castro_moura@hotmail.com>
2020-07-18 13:27:17 -0300

    Ajuste da regra de validacao do campo Matricula e correcao do script de carga automatica.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (0,'Xawoy', 'xawoy@tms.com.br', '7777', '123456', 'Engenharia da Computação');
-INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (1,'Furae', 'furae@tms.com.br', '7778', '678912', 'Ciência da Computação');
-INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (2,'Fupuy', 'fupuy@tms.com.br', '7779', '321654', 'Engenharia da Computação');
-INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (3,'Kuer', 'kuer@tms.com.br', '7780', '654987', 'Análise de Dados');
-INSERT INTO TB_ESTUDANTE (ID, EMAIL, NOME, TELEFONE, MATRICULA, CURSO) VALUES (4,'Blias', 'blias@tms.com.br', '7781', '666131', 'Ciência da Computação');
+INSERT INTO TB_ESTUDANTE (ID, NOME, EMAIL, TELEFONE, MATRICULA, CURSO) VALUES (0,'Xawoy', 'xawoy@tms.com.br', '7777', '123456', 'Engenharia da Computação');
+INSERT INTO TB_ESTUDANTE (ID, NOME, EMAIL, TELEFONE, MATRICULA, CURSO) VALUES (1,'Furae', 'furae@tms.com.br', '7778', '678912', 'Ciência da Computação');
+INSERT INTO TB_ESTUDANTE (ID, NOME, EMAIL, TELEFONE, MATRICULA, CURSO) VALUES (2,'Fupuy', 'fupuy@tms.com.br', '7779', '321654', 'Engenharia da Computação');
+INSERT INTO TB_ESTUDANTE (ID, NOME, EMAIL, TELEFONE, MATRICULA, CURSO) VALUES (4,'Blias', 'blias@tms.com.br', '7781', '666131', 'Ciência da Computação');
+INSERT INTO TB_ESTUDANTE (ID, NOME, EMAIL, TELEFONE, MATRICULA, CURSO) VALUES (3,'Kuer', 'kuer@tms.com.br', '7780', '654987', 'Análise de Dados');
